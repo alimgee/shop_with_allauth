9bd7b434500f89b076137104f9ad702eae03f2c3
Alan Mc Gee <mcgee.al@gmail.com>
2020-05-29 15:23:25 +0100

    adding templates to DIR in settings

--- ci_shop/settings.py
+++ ci_shop/settings.py
@@ -68,7 +68,8 @@ AUTHENTICATION_BACKENDS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

c8c41d9e3a6c967aafd6bfbbfafe0e3e2e593eaa
Alan mc Gee <mcgee.al@gmail.com>
2020-08-21 18:55:43 +0100

    Updated setting for media processor for tags

--- ci_shop/settings.py
+++ ci_shop/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'ci_shop.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTHENTICATION_BACKENDS = [
     
@@ -81,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
